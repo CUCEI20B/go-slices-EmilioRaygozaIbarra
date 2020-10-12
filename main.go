baf61dd160cbbef5a8d4acec3b40e5733c22aee8
EmilioRaygozaIbarra <emilio.raygoza@alumnos.udg.mx>
2020-10-12 12:20:17 -0400

    Slices

--- main.go
+++ main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var aux uint64
+	var suma uint64
+	var n uint64
+	var i uint64
+	var s []uint64
+	fmt.Scanln(&n)
+	for i=1;i<=n;i++{
+	  fmt.Scanln(&aux)
+	  s = append(s,aux)
+	}
+	for i=0;i<n;i++{
+	  aux = s[i]
+	  suma += aux
+	}
+	fmt.Println(suma)
 }
